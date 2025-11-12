443a70c90abc1b1cf59898ec794e66a86a0a72d2
sabba1010 <sabbahossain@gmaol.com>
2025-11-12 13:03:23 +0600

    /typeAssertion

--- src/typeAssertion.ts
+++ src/typeAssertion.ts
@@ -2,4 +2,27 @@ let anything: any;
 
 anything = 222;
 
-(anything as number).
+(anything as number).toFixed
+
+const kgToGMConverter = (input: string | number) => {
+
+
+    if (typeof input === 'number') {
+        return input * 1000;
+    } else if ( typeof input === 'string'){
+        const [value] = input.split(" ")
+        return `Converted output is: ${Number(value)
+            *1000}`
+    }
+
+}
+
+const result1 = kgToGMConverter(2);
+console.log(result1);
+
+const result2 = kgToGMConverter('2 kg') as 
+string;
+
+// result2.
+console.log(result2);
+
