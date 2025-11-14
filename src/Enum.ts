7d0968c7d828970273433ff11dae22822243a0db
sabba1010 <sabbahossain@gmaol.com>
2025-11-14 12:03:33 +0600

    Explore Enum

---
+++ src/Enum.ts
@@ -0,0 +1,19 @@
+// type UserRoles = "Admin" | "Editor" | "Viewer";
+
+
+enum UserRoles {
+    Admin = 'Admin',
+    Editor = 'Editor',
+    Viewer = 'Viewer',
+}
+
+const canEdit = (role: UserRoles) => {
+    if (role === UserRoles.Admin || role === UserRoles.Editor) {
+        return true;
+    } else {
+        return false;
+    }
+};
+
+const isEditPermissable = canEdit(UserRoles.Admin);
+console.log(isEditPermissable);
