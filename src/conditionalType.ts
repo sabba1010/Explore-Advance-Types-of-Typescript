dba25876db32ea5fb8bf9d44b08f610acd43ac66
sabba1010 <sabbahossain@gmaol.com>
2025-11-14 12:19:08 +0600

    conditional type

---
+++ src/conditionalType.ts
@@ -0,0 +1,23 @@
+
+
+type A = null;
+type B = undefined;
+
+type C = A extends number ? true : B extends undefined 
+ ? true : false ;
+
+
+ type RichPeoplelesVehicle = {
+    bike: string;
+    car: string;
+    ship: string;
+ };
+
+
+ type CheckVehicle<T> = T extends "bike" | "car" | "ship" ? 
+    true : false
+
+
+ type HasBike =  CheckVehicle<"bike"> ;
+ 
+
